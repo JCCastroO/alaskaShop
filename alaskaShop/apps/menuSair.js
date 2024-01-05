47931956e6aacbbc1c3e24b3af80642d494f3f27
João Carlos de Castro Oliveira <150449232+JCCastroO@users.noreply.github.com>
2024-01-05 20:42:46 -0300

    Add files via upload

--- alaskaShop/apps/menuSair.js
+++ alaskaShop/apps/menuSair.js
@@ -2,6 +2,12 @@ const btnLogado = document.querySelector('.navegacao-login');
 const menuSair = document.querySelector('[data-logado]')
 const deslogar = document.querySelector('[data-sair]');
 
+const novoProduto = document.querySelector('.navegacao-novoProduto');
+
 btnLogado.addEventListener('click', () => {
     menuSair.classList.toggle('hidden');
 });
+
+novoProduto.addEventListener('click', () => {
+    window.location.href = './adicionarProduto.html';
+})
